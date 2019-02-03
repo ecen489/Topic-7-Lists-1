43ed1b9b8838a083571e89951a8ef292391dce2e
sshakkot <sshakkot@tamu.edu>
2019-02-03 16:22:37 -0600

    Initial commit

---
+++ app/src/main/java/edu/tamu/ece/t7_lists1/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.tamu.ece.t7_lists1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
